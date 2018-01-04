a36e19eac36068e58c408c6328cb1189a9fae77d
Miles McLeod <milesmcleod123@gmail.com>
2018-01-04 10:39:25 -0300

    Complete heap structure

--- Miles_McLeod/lib/heap.rb
+++ Miles_McLeod/lib/heap.rb
@@ -1,9 +1,15 @@
+require 'byebug'
+
 class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
     @store = []
-    @prc = prc
+    if prc
+      @prc = prc
+    else
+      @prc = Proc.new { |x, y| x > y ? 1 : -1 }
+    end
   end
 
   def count
@@ -14,7 +20,7 @@ class BinaryMinHeap
     @store[0] = @store[-1]
     @store[-1] = temp
     return_val = @store.pop
-    BinaryMinHeap.heapify_down(@store, 0, @store.length, @prc)
+    BinaryMinHeap.heapify_down(@store, 0)
     return_val
   end
 
@@ -23,33 +29,69 @@ class BinaryMinHeap
   end
 
   def push(val)
+    # debugger
     @store.push(val)
     child_idx = @store.length - 1
-    BinaryMinHeap.heapify_up(@store, child_idx, @store.length, @prc)
+    BinaryMinHeap.heapify_up(@store, child_idx)
   end
 
   def self.child_indices(len, parent_index)
     left = (2 * parent_index) + 1
     right = left + 1
-    [left, right]
+    if left == len - 1
+      return [left]
+    else
+      return [left, right]
+    end
   end
 
   def self.parent_index(child_index)
+    raise 'root has no parent' if child_index == 0
     (child_index - 1) / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    return array if len == 1
+    prc ||= Proc.new { |x, y| x > y ? 1 : -1 }
+    child_indices = BinaryMinHeap.child_indices(len, parent_idx)
+    if child_indices.length == 1
+      child_idx = child_indices[0]
+    elsif prc.call(array[child_indices[0]], array[child_indices[1]]) == 1
+      child_idx = child_indices[1]
+    else
+      child_idx = child_indices[0]
+    end
+    while prc.call(array[parent_idx], array[child_idx]) == 1 && array[child_idx]
+      temp = array[parent_idx]
+      array[parent_idx] = array[child_idx]
+      array[child_idx] = temp
+      parent_idx = child_idx
+      child_indices = BinaryMinHeap.child_indices(len, parent_idx)
+      break if !array[child_indices[0]]
+      if child_indices.length == 1
+        child_idx = child_indices[0]
+      elsif prc.call(array[child_indices[0]], array[child_indices[1]]) == 1
+        child_idx = child_indices[1]
+      else
+        child_idx = child_indices[0]
+      end
+    end
+    array
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    return array if len == 1
+    prc ||= Proc.new { |x, y| x > y ? 1 : -1 }
     parent_idx = BinaryMinHeap.parent_index(child_idx)
-    while array[parent_idx] > array[child_idx] && child_idx != parent_idx
+    while prc.call(array[parent_idx], array[child_idx]) == 1 && child_idx != 0
       temp = array[parent_idx]
       array[parent_idx] = array[child_idx]
       array[child_idx] = temp
       child_idx = parent_idx
+      break if child_idx == 0
       parent_idx = BinaryMinHeap.parent_index(child_idx)
     end
+    array
   end
 end
 
